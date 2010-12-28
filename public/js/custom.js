68d650c8182147ac6060ec2ab50b949583f719b8
Sven Lito <me@svenlito.com>
2010-12-28 02:01:07 +0100

    markup tweaks and script additions

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,3 +1,13 @@
 jQuery(document).ready(function ($) {
-  
+var paper = Raphael('bg', '100%', '100%');
+
+
+
+paper.g.triangle(500, 300, 400).attr({
+  'fill': '#98E2B0',
+  'opacity': 0.8,
+  'stroke-width': 0
+}).rotate(45, true);
+
+
 });
