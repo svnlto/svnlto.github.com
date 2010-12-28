aef5bd52ec5aa74ae89a770898960832d7c7fab3
Sven Lito <me@svenlito.com>
2010-12-28 04:11:45 +0100

    working update

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,13 +1,28 @@
 jQuery(document).ready(function ($) {
-var paper = Raphael('bg', '100%', '100%');
+  
+  var r = Raphael('bg', '100%', '100%'), triangle, rectangle;
 
+  triangle = r.g.triangle(500, 300, 400).attr({
+    'fill': '#98E2B0',
+    'opacity': 0.8,
+    'stroke-width': 0
+  }).rotate(45, true);
+  
+  rectangle = r.rect(300, 105, 600, 25).attr({
+    fill: 'purple',
+    opacity: .4,
+    'stroke-width': 0
+  });
+  
 
-
-paper.g.triangle(500, 300, 400).attr({
-  'fill': '#98E2B0',
-  'opacity': 0.8,
-  'stroke-width': 0
-}).rotate(45, true);
-
+  $(triangle.node).click(function (e) {
+    var triangle = this.raphael;
+    
+    console.dir(triangle);
+    triangle.animate({
+      fill: 'purple', 
+      opacity: 1
+      }, 1000, "bounce");
+  });
 
 });
